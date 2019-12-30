7f308c61b1f582f7f3843aba8e1505a03660bfbd
ThakurAnupSingh7 <singhanup9412@gmail.com>
2019-12-30 12:39:15 +0530

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemMasterComponent } from './item-master/item-master.component';
+import { AssemblyComponent } from './assembly/assembly.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FinishingComponent } from './finishing/finishing.component';
+import { CostingComponent } from './costing/costing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path:'', pathMatch:'full', redirectTo:'item-master'},
+  {path:'item-master', component:ItemMasterComponent},
+  {path:'assembly', component:AssemblyComponent},
+  {path:'product-details', component:ProductDetailsComponent},
+  {path:'finishing', component:FinishingComponent},
+  {path:'costing', component:CostingComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
